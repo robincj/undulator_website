1686b44d1b8e73566ea0e68ed8744225a6dc618c
Robin CJ <robin@cameron-jones.net>
2015-04-29 13:51:42 +1200

    Rewrote A100 intro

--- information/A100_intro.php
+++ information/A100_intro.php
@@ -1,11 +1,8 @@
 <div class="text-center">
 	<h3>FRIDAY 6th to SUNDAY 8th NOVEMBER, 2015</h3>
 
-	<p>Taking place in the Aorangi mountain range, situated in the south-east of
-		the Wairarapa, the Aorangi Undulator 100 is a 100km 3 day stage race
-		starting on the Eastbourne coastline, South-East of Wellington,
-		traversing the Parangarahy Lakes area, the Orogorongo and Aorangi
-		Ranges to finish at Waikuku Lodge in the Aorangi forest park
-		South-East of the Wairarapa.</p>
+	<p> The <em>Aorangi Undulator 100</em> is a trail-running race in which competitors cover over 100km in 3 days.<br/>
+		It starts south-east of Wellington, on the Eastbourne coastline,
+		traverses the Parangarahu Lakes area, the Orogorongo Range and the mountainous Aorangi Forest Park, in south-eastern Wairarapa to finish at Waikuku Lodge.</p>
 
 </div>
