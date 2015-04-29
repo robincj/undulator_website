8932dc05f00d0dd760ef8451017ae724cc8d29ce
Robin CJ <robin@cameron-jones.net>
2015-04-29 13:18:06 +1200

    Corrected typo in intro

--- information/A100_intro.php
+++ information/A100_intro.php
@@ -1,7 +1,7 @@
 <div class="text-center">
 	<h3>FRIDAY 6th to SUNDAY 8th NOVEMBER, 2015</h3>
 
-	<p>Taking place Aorangi mountain range, situated in the south-east of
+	<p>Taking place in the Aorangi mountain range, situated in the south-east of
 		the Wairarapa, the Aorangi Undulator 100 is a 100km 3 day stage race
 		starting on the Eastbourne coastline, South-East of Wellington,
 		traversing the Parangarahy Lakes area, the Orogorongo and Aorangi
