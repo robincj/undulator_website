1b42e2d5e2547fa609907b461a90e33c54d258c0
robin <robin@manatee>
2015-05-06 22:13:00 +1200

    Embedded Mapmyrun map/elev iframe for AU.

--- information/maps.php
+++ information/maps.php
@@ -1,4 +1,4 @@
-<?php include 'piwik_track.php' ?>
+<?php include 'piwik_track.php'?>
 <div class="text-center">
 	<h2>Maps and Profiles</h2>
 	<p>
@@ -8,6 +8,17 @@
 			href="http://www.mapmyrun.com/nz/upper-hutt-wellington/aorangi-undulator-route-322964533">MapMyRun</a>.
 	</p>
 
+	<iframe id="mapmyfitness_route"
+		src="http://snippets.mapmycdn.com/routes/view/embedded/322964533?width=600&height=400&elevation=true&info=true&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=1&unit_type=metric&map_mode=TERRAIN&last_updated=2014-08-25T10:44:33+12:00&show_marker_every=2"
+		height="570px" width="100%" frameborder="0"></iframe>
+	<div style="text-align: right; padding-right: 20px;">
+		<!-- 
+		<a target="_blank" href="http://www.mapmyrun.com/routes/create/">Create
+			Maps</a> or find more routes in <a target="_blank"
+			href="/nz/upper-hutt-wellington/"> Upper Hutt </a> from millions at <a
+			href="http://www.mapmyrun.com">MapMyRun</a>
+			 -->
+	</div>
 	<!--
 	<h3>3D Video of Route <small>(requires Google-Earth plugin)</small></h3>
       <iframe width="100%" height="350px" scrolling="no" src="http://www.mapmyrun.com/routes/render_route_video?route_id=322964533">
@@ -16,8 +27,11 @@
    -->
 	<div class="row">
 		<div class="col-sm-6">
+			<!--
 			<a href="images/maps/elevation.jpg" target="_blank">
 				<img src="images/maps/elevation.jpg" /></a> 
+			 -->
+			  
 			<a href="images/maps/crap_map.jpg" target="_blank">
 				<img src="images/maps/crap_map.jpg" /></a>
 		</div>
@@ -26,5 +40,5 @@
 				<img src="images/maps/better_map.jpg" /></a>
 		</div>
 	</div>
-
+	
 </div>
