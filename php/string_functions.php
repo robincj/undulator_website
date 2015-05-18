405e9db8539b5f2b4b3c97e05421c8cf2965ad49
robincj <robin@cameron-jones.net>
2015-05-19 11:18:41 +1200

    Added string_functions.php containing ucname()

---
+++ php/string_functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Takes a string and uppercases the first characters of each word.
+ * Like ucwords except this works on hyphenated names or names with apostrophes in.
+ * Also handles Mc and Mac.
+ * 
+ * @param  (string) $text
+ * @return string
+ */
+function ucname($text) {
+	return preg_replace_callback ( "/(\w+)/", function ($m) {
+		$part = strtolower ( $m [0] );
+		if (preg_match ( "/^mc(.*)/", $part, $ending )) {
+			return "Mc" . ucfirst ( $ending [1] );
+		} elseif (preg_match ( "/^mac(.*)/", $part, $ending )) {
+			return "Mac" . ucfirst ( $ending [1] );
+		}
+		return ucfirst ( $part );
+	}, $text );
+}
