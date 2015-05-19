b76cfafa825cda5b82a908161dd03936ad14c550
robincj <robin@cameron-jones.net>
2015-05-19 13:57:01 +1200

    Added Salomon sponsor logo and link

--- links_col.php
+++ links_col.php
@@ -51,22 +51,29 @@ $(function () {
 			<small>Sponsors</small>
 			<div class="row">
 
-				<div class="col-xs-4 col-md-12">
+				<div class="col-xs-6 col-md-12">
 					<a href="http://www.tailwindnutrition.co.nz/"><img
 						class="img-responsive" width="165px"
 						src="images/sponsors/tailwind_nutrition_tr.png" /></a>
 				</div>
 
-				<div class="col-xs-4 col-md-12">
+				<div class="col-xs-6 col-md-12">
 					<a><img class="img-responsive" width="165px"
 						src="images/sponsors/lifestyle_sports_tr.png" /></a>
 				</div>
 
-				<div class="col-xs-4 col-md-12">
+				<div class="col-xs-6 col-md-12">
 					<a href="http://www.balancesportsnutrition.com/"><img
 						class="img-responsive" width="165px"
 						src="images/sponsors/balance_tr.png" /></a>
 				</div>
+				
+				<div class="col-xs-6 col-md-12">
+					<a href="http://www.salomon.com/"><img
+						class="img-responsive" width="125px"
+						src="images/sponsors/Salomon-Primary-Logo.black_lo_91512-small-tr.png" /></a>
+				</div>
+				
 			</div>
 		</div>
 	</div>
