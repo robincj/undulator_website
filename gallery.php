dbf8ae39c5743131f6dbf95a9775674446a000b3
robin <robin@manatee>
2015-05-19 22:00:46 +1200

    New tiled gallery started, with lightbox

--- gallery.php
+++ gallery.php
@@ -6,10 +6,15 @@ include_once 'php/createthumbs.php';
 $originals = "images/photos/originals";
 $photodir = "images/photos";
 $thumbdir = "images/photos/thumbs";
-createThumbs ( $originals, $photodir, 1140 );
+createThumbs ( $originals, $photodir, 600 );
 createThumbs ( $originals, $thumbdir, 100 );
 echo "<div>";
-include 'js/jssor_bootstrap/php/jssor_image-gallery.php';
+//include 'js/jssor_bootstrap/php/jssor_image-gallery.php';
+include 'tiler/tiler.php';
+$tiler = new Tiler();
+$tiler->photodir = $photodir;
+$tiler->photodir_url = "/images/photos";
+$tiler->show();
 echo "</div>";
 
 ?>
---
+++ tiler/tiler.php
@@ -0,0 +1,53 @@
+
+<?php
+class Tiler {
+	public $photodir = "../images/photos";
+	public $photodir_url = "/images/photos";
+	public $bigside = 300;
+	/*
+	 * getimagesize Array ( [0] => 1140 [1] => 1900 [2] => 2 [3] => width="1140" height="1900" [bits] => 8 [channels] => 3 [mime] => image/jpeg )
+	 */
+	public function show() {
+		$bigside = $this->bigside;
+		$photodir_url = $this->photodir_url;
+		?>
+<script type="text/javascript" src="/lightbox/dist/ekko-lightbox.min.js"></script>
+<script>
+		$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    		event.preventDefault();
+   			$(this).ekkoLightbox();
+		});
+	</script>
+<style>
+.tiler {
+	margin: 3px;
+}
+</style>
+<div id='tiler'>
+		<?php
+		foreach ( scandir ( $this->photodir ) as $file ) {
+			if (! preg_match ( "/(jpg|png|jpeg)$/i", $file ))
+				continue;
+			$size = getimagesize ( $this->photodir . "/$file" );
+			$width = $size [0];
+			$height = $size [1];
+			$ratio = $width / $height;
+			if ($ratio >= 1) {
+				$display_width = $bigside;
+				$display_height = $height * ($bigside / $width);
+			} else {
+				$display_width = $width * ($bigside / $height);
+				$display_height = $bigside;
+			}
+			echo <<<EOH
+		<span class="tiler" >
+		<a href="$photodir_url/$file" data-toggle="lightbox" data-gallery="multiimages" data-title="Aorangi Undulator" >                              
+			<img src="$photodir_url/$file" width="$display_width" height="$display_height" />
+		</a>								
+		</span>
+EOH;
+		}
+		echo "</div>";
+	}
+}
+?>
