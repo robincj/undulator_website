2e93042de1f76e6ef65382ca83d24509cc7c9e0d
robincj <robin@cameron-jones.net>
2015-05-29 09:47:03 +1200

    new quote

--- information/quotes.php
+++ information/quotes.php
@@ -27,12 +27,15 @@
 <script src="js/quotebox.js"></script>
 
 <script>
+var qq = '"';
+var q="'";
 var quotes = [{ quote: "We had seen God in His splendors, heard the text that Nature renders. We had reached the naked soul of man.", author: "Earnest Shakleton" },
 { quote: "You have to go look for happiness in life, find it in the things that make you feel alive. Life is not something to be preserved or protected, it is to explored and lived to the full.", author: "Kilian Jornet"},
 { quote: "Impossible is just a word that makes me try harder" },
+{ quote: "The only one who can tell you "+qq+"You can't"+qq+" is you, and you don't have to listen.<br>Unless it's your wife." },
 { quote: "Strong is what you have left when you have used up all your weak." },
 { quote: "Don't trust the grown ups! You need courage. You have to be a rebel." },
-{ quote: "Why does one do these things? It's definitely a powerful experience. In some ways it's a very pointless but at one time, not so long ago there was enough risk in life that you did not want to go out pursuing these things for fun whereas now everything is so safe and sanitised and boring that there are some people myself included who feel the need to experience the danger , the apprehension, the sensations, the power, the decision making…you know.  With your seat belt on and your central heating turned up you need to get away from all that .............to get back to something a little more fundamental.", author: "Leo Holding" },
+{ quote: "Why does one do these things? It's definitely a powerful experience. In some ways it's a very pointless but at one time, not so long ago there was enough risk in life that you did not want to go out pursuing these things for fun whereas now everything is so safe and sanitised and boring that there are some people myself included who feel the need to experience the danger , the apprehension, the sensations, the power, the decision makingâ€¦you know.  With your seat belt on and your central heating turned up you need to get away from all that .............to get back to something a little more fundamental.", author: "Leo Holding" },
 { quote: "Most people would be better off with more pain in their lives" },
 { quote: "The only distance you need to conquer in this race is 5 inches....The five inches between your ears." },
 { quote: "You are a middle aged man with a heart of a lion." },
@@ -68,7 +71,7 @@ var quotes = [{ quote: "We had seen God in His splendors, heard the text that Na
 { quote: "If you exercise your body you also look after your mind." },
 { quote: "The 'Comfort Zone' is the problem in life. Imagine you wake on a rainy morning. The rain bangs on your windows. It's easier to stay in your nice bed than go for a walk in the rain. But in the evening you think 'what a boring day.<br/>If you do it, if you get out of bed and walk in the rain then you will look back on the day and think 'what a wonderful day." },
 { quote: "Comfort really is a bad thing. We are striving for comfort all the time. But it kills you." },
-{quote: "Mountains are made to look down from, not up at – so get on with it", author: "Sherman" },
+{quote: "Mountains are made to look down from, not up at â€“ so get on with it", author: "Sherman" },
 {quote: "Try to be a rainbow in someone's cloud."}	]
 ;
 
