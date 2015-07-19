c29848a9eb32d3b18f48291131fdc4335229cc19
robincj <dev@cameron-jones.net>
2015-07-19 21:43:38 +1200

    Twitter feed added

--- share_icons.php
+++ share_icons.php
@@ -49,7 +49,7 @@ a.share-youtube:hover {
 
 <br />
 <div class="row">
-	<div class="col-xs-4 col-sm-12">
+	<div class="col-xs-6 text-center">
 		<a class="share-icon share-facebook" target="_blank"
 			href="http://www.facebook.com/aorangiundulator"
 			title="Undulator on Facebook"></a>
@@ -61,15 +61,23 @@ a.share-youtube:hover {
 		href="https://www.facebook.com/groups/1548916308660466/"
 		title="A100 on Facebook"></a>
 	</div>
-	 -->
+
 	<div class="col-xs-4 col-sm-12">
 		<a class="share-icon share-twitter" target="_blank"
 			href="https://twitter.com/intent/follow?region=follow_link&screen_name=undulator0109&tw_p=followbutton"
 			title="Follow on Twitter"></a>
 	</div>
-	<div class="col-xs-4 col-sm-12">
+		 -->
+	<div class="col-xs-6 text-center">
 		<a class="share-icon share-youtube" target="_blank"
 			href="http://www.youtube.com/watch?v=x3mVNYRViv4"
 			title="Undulator on YouTube"></a>
+	</div>
+
+</div>
+
+<div class="row">
+	<div class="col-xs-12">
+	<?php include 'twitter_feed.php';?>
 	</div>
 </div>
