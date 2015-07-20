123fbb24b0fe407d2e6b9ac8d5c87b2fb3c49de0
robincj <dev@cameron-jones.net>
2015-07-20 21:30:29 +1200

    Switched back to twitter feed for @undulator0109 sent messages only

--- twitter_feed.php
+++ twitter_feed.php
@@ -1,10 +1,10 @@
 
 
-<!-- 
+
 <a class="twitter-timeline" href="https://twitter.com/undulator0109" data-widget-id="622698495560921088">Tweets by @undulator0109</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
--->
 
+<!-- 
 <a class="twitter-timeline" href="https://twitter.com/search?q=%40undulator0109%20%23aorangiundulator%20%23a100" data-widget-id="622902189099368452">Tweets about @undulator0109 #aorangiundulator #a100</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
+-->
