e6e2947dcde3e0a323e9b55759661a0ef1c62e60
robincj <dev@cameron-jones.net>
2015-11-23 21:54:07 +1300

    Added link to Lorrie's blog

--- youtube.php
+++ youtube.php
@@ -1,4 +1,11 @@
 <?php include 'piwik_track.php' ?>
+
+<h3>Articles & Celebrity Endorsements</h5>
+<p>
+<a href="https://lorrylovestorun.wordpress.com/2015/11/08/aorangi-undulator-7th-november-2015/" >
+LorryLovesToRun Blog</a>
+</p>
+<h3>Videos</h3>
 <h5>Video footage from the 2013 event</h5>
 <iframe width="560" height="315" class="embed-responsive-item"
 	src="https://www.youtube.com/embed/x3mVNYRViv4" frameborder="0"
