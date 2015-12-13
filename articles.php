d8083ffbb9d05b0dd22dfc22c3da00cf75d12bfc
robincj <dev@cameron-jones.net>
2015-12-13 21:08:24 +1300

    Added Sharron's blog.  Fixed random photo selection so it always does 7.

--- articles.php
+++ articles.php
@@ -2,6 +2,12 @@
 
 <h3>Articles & Celebrity Endorsements</h5>
 <p>Visit these sites to find out what previous participants have to say about the Undulator events.</p>
+
+<p>
+<a href="http://mustlovehills.com/2015/12/12/eat-drink-run-repeat-a100-race-report-2/" target="_blank" >
+Sharron's Blog</a>
+</p>
+
 <p>
 <a href="https://lorrylovestorun.wordpress.com/2015/11/08/aorangi-undulator-7th-november-2015/" target="_blank" >
 LorryLovesToRun Blog</a>
@@ -11,3 +17,4 @@ LorryLovesToRun Blog</a>
 <a href="https://gaztom77.wordpress.com/2015/12/08/the-aorangi-100/" target="_blank" >
 Gareth's Blog</a>
 </p>
+
--- js/jssor_bootstrap/php/jssor_bootstrap_slider.php
+++ js/jssor_bootstrap/php/jssor_bootstrap_slider.php
@@ -21,31 +21,37 @@
 		style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1140px; height: 642px; overflow: hidden; background-color: #000;">
 	<?php
 	$photodir = "images/photos/2015";
-	$rands = array ();
-	for($i = 1; $i <= 7; $i ++) {
-		$rands [] = rand ( 1, 192 );
-	}
+	$displayNum = 7;
 	// $photolist = explode(", ", "10689813_10152947587125695_4811765621027737093_n.jpg, 20.jpg, 10619916_10154865765765287_6749377587880673130_o.jpg, 5.jpg, A100_undies.jpg, image006.jpg, 11203537_977989312212363_6885734874301767830_o.jpg");
-	
-	$count = 0;
+	$imgFileList = array ();
+	$numImages = 0;
 	foreach ( scandir ( $photodir ) as $filename ) {
-		// foreach ( $photolist as $file ) {
 		if (! preg_match ( "/(jpg|png|jpeg)$/i", $filename ))
 			continue;
+		
+		$numImages ++;
 		$file = "$photodir/$filename";
 		// We only want portrait style photos
-		list ( $width, $height ) = getimagesize ( $file );		
+		list ( $width, $height ) = getimagesize ( $file );
 		if ($height > $width)
 			continue;
-		$count ++;
-		if (in_array ( $count, $rands )) {
-			echo <<<EOH
+		
+		$imgFileList [] = "$photodir/$filename";
+	}
+	
+	// Randomly pick $displayNum photos from the list
+	$count = 0;
+	while ( $count < $displayNum && $cont <= count ( $imgFileList ) ) {		
+		$rand = rand ( 1, count ( $imgFileList ) - 1 );
+		$file = $imgFileList [$rand];
+		echo <<<EOH
 							<div>
 							<img u="image" src2="$file" />
 							</div>
 EOH;
-		}
+		$count++;
 	}
+	
 	?>							
 						</div>
 	<!-- Bullet Navigator Skin Begin -->
