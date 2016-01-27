b73f5b5f571928a0eb72ab7efc4950dddbad9744
robincj <dev@cameron-jones.net>
2016-01-27 20:37:05 +1300

    Added quote

--- information/quotes.php
+++ information/quotes.php
@@ -77,6 +77,7 @@ var quotes = [{ quote: "We had seen God in His splendors, heard the text that Na
 {quote: "You have the key to the hurt locker.<br/>Open it."},
 {quote: "A few people live the dream but the some of us just snatch glimpses of it.", author: "A Shelton"},
 {quote: "Most people convince themselves are all grown up but deep down they are not.", author: "A Shelton"},
+{quote: "It's a terrible thing, I think, in life to wait until you're ready. I have this feeling now that actually no one is ever ready to do anything. There is almost no such thing as ready. There is only now. And you may as well do it now. Generally speaking, now is as good a time as any.", author: "Hugh Laurie"},
 ]
 ;
 
