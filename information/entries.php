7ad49d6baefb6de53e7c239be0e511a0702912ea
Robin CJ <robin@cameron-jones.net>
2016-09-10 22:08:59 +1200

    ignore blank lines in entries csv files

--- information/entries.php
+++ information/entries.php
@@ -111,7 +111,7 @@ function entries_table($title, $csvfile, $max_entries) {
 <?php
 	$count = 1;
 	foreach ( array_map ( 'str_getcsv', file ( $csvfile ) ) as $entry ) {
-		if (! $entry || preg_match ( '/^#/', reset ( $entry ) ))
+		if (! $entry || preg_match ( '/^#/', reset ( $entry ) ) || preg_match ( '/^\s*$/', reset ( $entry ) ))
 			continue;
 			// Fields:
 			// NAME,EMAIL,CATEGORY,PAID,EXPERIENCE,WAITLIST,AGE,GENDER,PREDICTED,T-SIZE,T-QUANTITY,FEE,LOCATION,MEDICAL
@@ -147,7 +147,7 @@ function waitlist_table($title, $csv, $max_entries) {
 	$count = 1;
 	if (! is_array ( $csv )) {
 		foreach ( array_map ( 'str_getcsv', file ( $csv ) ) as $entry ) {
-			if (! $entry || preg_match ( '/^#/', reset ( $entry ) ))
+			if (! $entry || preg_match ( '/^#/', reset ( $entry ) ) || preg_match ( '/^\s*$/', reset ( $entry ) ))
 				continue;
 			if ($count ++ <= $max_entries)
 				continue;
