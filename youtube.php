db2cfe850d991bd44bd98ac91bbd5fa42b56e07e
robincj <dev@cameron-jones.net>
2016-11-10 22:37:38 +1300

    2016 youtube video links

--- youtube.php
+++ youtube.php
@@ -1,10 +1,19 @@
 <?php include 'piwik_track.php' ?>
 
 <h3>Videos</h3>
+<h5>Finish line video footage from the 2016 event</h5>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/t71feWmDduU" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ieLSaoMn--w" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hDkPMpaoqAU" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/chSaFqcJtuM" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qPurIYhqdtc" frameborder="0" allowfullscreen></iframe>
+
+
+<h5> Viktor's 2014 Dance Rave Footage</h5>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/IB0bK2qsY1s" frameborder="0" allowfullscreen></iframe>
+
+
 <h5>Video footage from the 2013 event</h5>
 <iframe width="560" height="315" class="embed-responsive-item"
 	src="https://www.youtube.com/embed/x3mVNYRViv4" frameborder="0"
 	allowfullscreen></iframe>
-
-<h5> Viktor's 2014 Dance Rave Footage</h5>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/IB0bK2qsY1s" frameborder="0" allowfullscreen></iframe>
