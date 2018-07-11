4dad160ed7367ce225cbb72e8434b336efcc07e7
robincj <dev@cameron-jones.net>
2018-07-11 22:01:10 +1200

    CSS tidying

--- share_icons.php
+++ share_icons.php
@@ -1,66 +1,15 @@
-<style>
-.share-icon-block {
-	white-space: nowrap;
-	display: inline;
-	/* position: absolute; 
-	
-	top: 10px;
-	right: 5px;
-	width: 40px;
-	height: 160px;
-	text-align: right; */
-}
 
-.share-icon {
-	display: inline-block;
-	margin: 4px;
-	width: 32px;
-	height: 32px;
-	cursor: pointer;
-	vertical-align: middle;
-	background-image: url(/images/logos/share-icons.png);
-	/* float: right; */
-}
+<a class="share-icon share-facebook" target="_blank"
+	href="http://www.facebook.com/aorangiundulator"
+	title="Undulator on Facebook"></a>
+<a class="share-icon share-youtube" target="_blank"
+	href="http://www.youtube.com/watch?v=x3mVNYRViv4"
+	title="Undulator on YouTube"></a>
+<a class="share-icon share-twitter" target="_blank"
+	href="https://twitter.com/intent/follow?region=follow_link&screen_name=undulator0109&tw_p=followbutton"
+	title="Follow on Twitter"></a>
 
-.share-twitter {
-	background-position: -40px 0;
-}
-
-a.share-twitter:hover {
-	background-position: -40px -40px;
-}
-
-.share-facebook {
-	background-position: 0 0;
-}
-
-a.share-facebook:hover {
-	background-position: 0 -40px;
-}
-
-.share-youtube {
-	background-position: -200px 0;
-}
-
-a.share-youtube:hover {
-	background-position: -200px -40px;
-}
-</style>
-
-<br />
-<div class="row">
-	<div class="col-xs-2 text-center">
-		<a class="share-icon share-facebook" target="_blank"
-			href="http://www.facebook.com/aorangiundulator"
-			title="Undulator on Facebook"></a>
-		<a class="share-icon share-youtube" target="_blank"
-			href="http://www.youtube.com/watch?v=x3mVNYRViv4"
-			title="Undulator on YouTube"></a>
-		<a class="share-icon share-twitter" target="_blank"
-			href="https://twitter.com/intent/follow?region=follow_link&screen_name=undulator0109&tw_p=followbutton"
-			title="Follow on Twitter"></a>
-	</div>
-	<!--  
+<!--  
 	<div class="col-xs-4 col-md-12">
 		<a
 		class="share-icon share-facebook" target="_blank"
@@ -80,11 +29,10 @@ a.share-youtube:hover {
 	</div>
 		 -->
 
-</div>
 <!-- 
 <div class="row">
 	<div class="col-md-12">
-	<?php include 'twitter_feed.php';?>
+	<?php //include 'twitter_feed.php';?>
 	</div>
 </div>
  -->
