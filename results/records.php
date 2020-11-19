f56ba80e6490c7a6182a6d46b5053690ec5d5803
robincj <robin@cameron-jones.net>
2020-11-19 22:03:32 +1300

    Course record updates

--- results/records.php
+++ results/records.php
@@ -1,24 +1,77 @@
 <?php include 'piwik_track.php'?>
 
 
+<h2>A100 3-Day Total Course Records</h2>
+<div class="table-responsive">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Competitor</th>
+				<th>Event/Category</th>
+				<th>Year</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Tim Sutton</td>
+				<td>A100/OM</td>
+				<td>2015</td>
+				<td>10:57:00</td>
+			</tr>
+			<tr>
+				<td>Joe Murphy</td>
+				<td>A100/VM</td>
+				<td>2020</td>
+				<td>12:43:57</td>
+			</tr>
+			<tr>
+				<td>Lou Beckingsale</td>
+				<td>A100/OW</td>
+				<td>2015</td>
+				<td>13:28:00</td>
+			</tr>
+			<tr>
+				<td>Marta Zanetti</td>
+				<td>A100/VW</td>
+				<td>2015</td>
+				<td>16:14:00</td>
+			</tr>
+			<tr>
+				<td>Sue Crowley</td>
+				<td>A100/SVW</td>
+				<td>2019</td>
+				<td>15:08:00</td>
+			</tr>
+			<tr>
+				<td>Iain Atkinson</td>
+				<td>A100/SVM</td>
+				<td>2017</td>
+				<td>16:00:00</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 <h2>A100 Day 1 Course Records</h2>
 <div class="table-responsive">
-        <table class="table table-hover">
-                <thead>
-                        <tr>
-                                <th>Competitor</th>
-                                <th>Event/Category</th>
-                                <th>Time</th>
-                        </tr>
-                </thead>
-                <tbody>
-                        <tr>
-                                <td>Tim Sutton</td>
-                                <td>AU/OM</td>
-                                <td>4:14:00</td>
-                        </tr>
-                </tbody>
-        </table>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Competitor</th>
+				<th>Event/Category</th>
+				<th>Year</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Tim Sutton</td>
+				<td>A100/OM</td>
+				<td>2015</td>
+				<td>4:14:00</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
 
 
@@ -29,6 +82,7 @@
 			<tr>
 				<th>Competitor</th>
 				<th>Event/Category</th>
+				<th>Year</th>
 				<th>Time</th>
 			</tr>
 		</thead>
@@ -36,52 +90,56 @@
 			<tr>
 				<td>Chris Swallow</td>
 				<td>AU/OM</td>
+				<td>2016</td>
 				<td>3:57:00</td>
 			</tr>
 			<tr>
-				<td>Tim Sutton</td>
-				<td>A100/OM</td>
-				<td>4:19:00</td>
+				<td>Martin McCrudden</td>
+				<td>AU/YM</td>
+				<td>2017</td>
+				<td>4:33:00</td>
+			</tr>
+			<tr>
+				<td>Andrew Thompson</td>
+				<td>AU/VM</td>
+				<td>2019</td>
+				<td>4:12:00</td>
 			</tr>
 			<tr>
 				<td>Lou Beckingsale</td>
 				<td>A100/OW</td>
+				<td>2015</td>
 				<td>5:04:00</td>
 			</tr>
 			<tr>
-				<td>Steve Neary</td>
-				<td>A100/VM</td>
-				<td>5:12:00</td>
-			</tr>
-			<tr>
-				<td>William Jones</td>
-				<td>AU/YM</td>
-				<td>5:19:00</td>
+				<td>Will Parker</td>
+				<td>AU/SVM</td>
+				<td>2019</td>
+				<td>5:05:00</td>
 			</tr>
 			<tr>
 				<td>Jackie Holley</td>
 				<td>AU/VW</td>
+				<td>2014</td>
 				<td>5:22:00</td>
-			</tr>
-			<tr>
-				<td>Adrian Jurke</td>
-				<td>AU/SVM</td>
-				<td>5:37:00</td>
 			</tr>
 			<tr>
 				<td>Jill Westenra</td>
 				<td>AU/SVW</td>
+				<td>2014</td>
 				<td>5:39:00</td>
 			</tr>
 			<tr>
 				<td>Georgia Whiting</td>
 				<td>AU/YW</td>
-				<td>6:09:00</td>
+				<td>2020</td>
+				<td>6:07:00</td>
 			</tr>
 			<tr>
-				<td>Andrew Shackleton</td>
+				<td>Wayne Ricketts</td>
 				<td>AU/SDVM</td>
-				<td>8:21:00</td>
+				<td>2015</td>
+				<td>7:18:00</td>
 			</tr>
 		</tbody>
 	</table>
@@ -146,20 +204,22 @@
 
 <h2>A100 Day 3 Course Records</h2>
 <div class="table-responsive">
-        <table class="table table-hover">
-                <thead>
-                        <tr>
-                                <th>Competitor</th>
-                                <th>Event/Category</th>
-                                <th>Time</th>
-                        </tr>
-                </thead>
-                <tbody>
-                        <tr>
-                                <td>Tim Sutton</td>
-                                <td>AU/OM</td>
-                                <td>2:24:00</td>
-                        </tr>
-                </tbody>
-        </table>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Competitor</th>
+				<th>Event/Category</th>
+				<th>Year</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Tim Sutton</td>
+				<td>AU/OM</td>
+				<td>2015</td>
+				<td>2:24:00</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
